d600ba03ef40308e4f919a5fb0047fdc30319857
Colin <colingilbert86@gmail.com>
2023-02-02 13:26:38 -0600

    Added scoring logic

---
+++ GUI/GUI.gd
@@ -0,0 +1,6 @@
+extends CanvasLayer
+
+
+func update_score(player, score):
+	var score_label = get_node("Banner/HBoxContainer/Player%sScore" % player)
+	score_label.text = str(score)
--- Levels/Level.gd
+++ Levels/Level.gd
@@ -1,17 +1,31 @@
 extends Spatial
 
+var Player1Score = 0
+var Player2Score = 0
 
 func _on_GoalDetector_body_entered(body, goal_id):
 	var scoring_player
 	if goal_id == 1:
-		scoring_player = "2"
+		scoring_player = 2
 	else:
-		scoring_player = "1"
-	print("Player " + scoring_player + " has scored a goal!" )
+		scoring_player = 1
+	#print("Player " + str(scoring_player) + " has scored a goal!" )
 	get_tree().call_group("game_pieces", "freeze")
 	$Timer.start()
+	update_score(scoring_player)
 
 
 func _on_Timer_timeout():
 	get_tree().call_group("game_pieces", "reset")
 	$Timer.stop()
+
+func update_score(player):
+	var new_score
+	match player:
+		1:
+			Player1Score += 1
+			new_score = Player1Score
+		2:
+			Player2Score += 1
+			new_score = Player2Score
+	$GUI.update_score(player, new_score)
